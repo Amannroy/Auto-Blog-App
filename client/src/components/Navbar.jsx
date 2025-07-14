6780974082035d4eb4313f6b4166b3dd31d51770
Aman Roy <royaman56456@gmail.com>
2025-07-14 22:22:35 +0530

    Committing Blog AI APP Project

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:xm-32'>
+        <img src={""} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
